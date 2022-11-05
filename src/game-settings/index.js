07539366a55cd3c55b134a044a26a2eace4020a8
Benedict Schurwanz <19333755+benedictschurwanz@users.noreply.github.com>
2022-11-05 15:53:01 -0500

    add game settings values for exporting to reducers' initial states

---
+++ src/game-settings/index.js
@@ -0,0 +1,10 @@
+// Variables denoting settings for the game
+
+export const starting_fleet = {
+	"Carrier": 5,
+	"Battleship": 4,
+	"Cruiser": 3,
+	"Destoryer": 3,
+	"Patrol boat": 2,
+}
+export const number_of_boats = starting_fleet.length;
