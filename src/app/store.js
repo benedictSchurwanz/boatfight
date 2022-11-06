76c9176ff2566890f8c9675bd2637eae3406aaae
Benedict Schurwanz <19333755+benedictschurwanz@users.noreply.github.com>
2022-11-05 19:14:37 -0500

    create Game data and Player data in Redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import playerReducer from '../store/playerData'
+import gameReducer from '../store/gameData'
 
 export const store = configureStore({
-	reducer: {}
+	reducer: {
+		game: gameReducer,
+		player: playerReducer, // this is playerSlice.reducer from playerSlice.js
+	}
 })
