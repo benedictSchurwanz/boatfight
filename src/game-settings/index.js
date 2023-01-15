4e91396aa4b4b92fcbb5621565d160b3f43206ff
Benedict Schurwanz <19333755+benedictschurwanz@users.noreply.github.com>
2023-01-14 20:55:50 -0600

    remove redux toolkit

--- src/game-settings/index.js
+++ src/game-settings/index.js
@@ -7,4 +7,4 @@ export const starting_fleet_lengths = {
 	"Submarine": 3,
 	"Destroyer": 2,
 }
-export const fleet_size = starting_fleet_lengths.length;
+// export const fleet_size = starting_fleet_lengths.length; // this should be set or derived elsewhere
