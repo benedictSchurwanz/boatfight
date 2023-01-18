6550c0daa831a84bc7681177135b69814d45b385
Benedict Schurwanz <19333755+benedictschurwanz@users.noreply.github.com>
2023-01-17 19:03:26 -0600

    add some wrappers

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
-import { store } from '.app/store'
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux'
 
+import { store } from '.app/store'
+import App from './App';
+
+import './index.css';
+
+
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+  <Router>
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  </Router>
+  , document.getElementById('root')
 );
+
+// Do Next:
+// 
