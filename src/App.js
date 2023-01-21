b6a4e6452e12f7528d05450a1cec334da178269e
Benedict Schurwanz <19333755+benedictschurwanz@users.noreply.github.com>
2023-01-20 19:40:07 -0600

    add components for title, container and board

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 // Dependencies
 import React from "react";
+import styled from "styled-components";
 
 // CSS
 import './index.css';
 
+// Styled Components
+import { 
+  Container,
+  BoardContainer,
+  Title
+ } from "./styles/styled-components"
+
 function App() {
   return (
-    <div>
-      <h1>BoatFight</h1>
-    </div>
+    <Container>
+      <Title>BoatFight</Title>
+      <BoardContainer>Hello.</BoardContainer>
+    </Container>
   );
 }
 
 export default App;
 
 /* Do Next:
+- style BoardContainer
+- put it in App
+- have a look
 
-
+Title: 
+  - align: center
 */
---
+++ src/styles/styled-components.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+	width: auto;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`
+export const Title = styled.h1`
+	width: auto;
+`
+
+// Board
+export const BoardContainer = styled.div`
+	width: 60%;
+	align: left;
+	// height: 2;
+  background-color: light-blue;
+`
