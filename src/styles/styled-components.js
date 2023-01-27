7fa9f969c62dd910d1c7a73e89ca0f1dd170e3a2
Benedict Schurwanz <19333755+benedictschurwanz@users.noreply.github.com>
2023-01-26 21:37:23 -0600

    board css tweaks

--- src/styles/styled-components.js
+++ src/styles/styled-components.js
@@ -20,21 +20,25 @@ export const Title = styled.h1`
 export const BoardContainer = styled.div`
 	// border: 1px solid red;
 	
-	width: 50vw;
-	height: 50vw;
+	// width: 50vw;
+	// height: 50vw;
+	width: 550px;
+	height: 440px;
+	
 	align-self: flex-start;
   background-color: lightblue;
 	border-radius: 1em;
 	
 	margin: 1em;
-	padding: 1em;
-	padding-left: 2.5em;
-	padding-right: 0em;
+	padding: 1rem;
+	padding-top: 0.2rem;
+	padding-left: 1rem;
+	padding-right: 0rem;
 	
 	* {
 		white-space: pre-wrap;
 		font-family: monospace;
-		font-size: 1.15em;
-		line-height: 1.8em;
+		font-size: 0.825rem;
+		line-height: 1.175rem;
 	}
 `
