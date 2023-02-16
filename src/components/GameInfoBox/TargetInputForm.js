10e794f658e8410307f60b6f75f31571e25948df
Benedict Schurwanz <19333755+benedictschurwanz@users.noreply.github.com>
2023-02-16 10:16:17 -0600

    uninstalled antd

--- src/components/GameInfoBox/TargetInputForm.js
+++ src/components/GameInfoBox/TargetInputForm.js
@@ -1,20 +1,9 @@
 import React from "react";
 import { FormContainer } from "../../styles";
-import { Button, Form, Input } from 'antd';
-
-const finishHandler = () => {
-	console.log('Success. ')
-}
 
 const TargetInputForm = () => {
   return (
 		<FormContainer>
-			<Form 
-				name="target-input-form"
-				onFinish={finishHandler}
-			>
-				
-			</Form>
 		</FormContainer>
 	);
 };
