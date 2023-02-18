238762ffba75f43e4b4cc8f19024d207cd634790
Benedict Schurwanz <19333755+benedictschurwanz@users.noreply.github.com>
2023-02-17 20:37:14 -0600

    added some initial state objects for game data structures

--- src/App.js
+++ src/App.js
@@ -11,7 +11,35 @@ import "./index.css";
 // Styled Components
 import { AppContainer, Title, GameContainer } from "./styles";
 
+const initialGameState = {
+  game_id: 0,
+  player1: {},
+  player2: {},
+  score: {},
+  winner: 0
+}
+
+const initialPlayerState = {
+  player_id: 0,
+  player_name: "",
+  board: [],
+  fleet: [],
+  boats_floating: null,
+}
+
+const initialFleetState = []
+
+const initialBoatState = {
+  boat_name: "",
+  boat_length: 0,
+  boat_status: [], // array containing the squares of the boat, containing hit/miss/null
+}
+
+
+
 function App() {
+  
+  
   return (
     <AppContainer>
       <Title>BoatFight</Title>
@@ -24,7 +52,3 @@ function App() {
 }
 
 export default App;
-
-/* Do Next:
-
-*/
