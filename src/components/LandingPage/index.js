977d048e798eb660d06f8a6cde611eb23814d4a9
Benedict Schurwanz <19333755+benedictschurwanz@users.noreply.github.com>
2023-02-21 17:26:50 -0600

    add LandingPage component

---
+++ src/components/LandingPage/index.js
@@ -0,0 +1,17 @@
+// Dependencies
+import React from "react";
+import { Link } from "react-router-dom";
+// Styles, mui
+import { Button } from "@mui/material";
+
+const LandingPage = () => {
+  return (
+    <Link to="/game">
+      <Button variant="contained" color="standard">
+        Start Game
+      </Button>
+    </Link>
+  );
+};
+
+export default LandingPage;
