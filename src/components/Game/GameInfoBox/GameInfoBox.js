59436a5ea320edde6f336b9286f86df5b6a5cf34
Benedict Schurwanz <19333755+benedictschurwanz@users.noreply.github.com>
2023-02-21 17:24:27 -0600

    directory structure

---
+++ src/components/Game/GameInfoBox/GameInfoBox.js
@@ -0,0 +1,19 @@
+// Dependencies
+import React from "react";
+// Styles
+import { GameInfoContainer, GameInfoHeader } from "../../../styles";
+// Components
+import BoatList from "./BoatList";
+import TargetInputForm from "./TargetInputForm";
+
+const GameInfoBox = () => {
+  return (
+    <GameInfoContainer>
+      <GameInfoHeader>Your Fleet</GameInfoHeader>
+      <BoatList />
+      <TargetInputForm />
+    </GameInfoContainer>
+  );
+};
+
+export default GameInfoBox;
