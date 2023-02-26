292d0e911de8b27cd541693ad57740b63ee1ce27
Benedict Schurwanz <19333755+benedictschurwanz@users.noreply.github.com>
2023-02-26 12:10:57 -0600

    add BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 // Dependencies
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 // Components
-import App from './App';
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
-
-// Do Next:
-// reducer, middlewares.
