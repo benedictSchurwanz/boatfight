506af06e9a659fd324d2f19a4743bc7839d5712c
Benedict Schurwanz <19333755+benedictschurwanz@users.noreply.github.com>
2023-02-26 12:09:59 -0600

    refactor App component to use Routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 // Dependencies
 import React from "react";
-
+import { Routes, Route } from "react-router";
 // React Components
-import Board from "./components/Board/Board";
-import GameInfoBox from "./components/GameInfoBox/GameInfoBox.js";
-
-// CSS
+import LandingPage from "./components/LandingPage"
+import Game from "./components/Game"
+// Styles
+import { AppContainer, Title } from "./styles";
 import "./index.css";
-
-// Styled Components
-import { AppContainer, Title, GameContainer } from "./styles";
 
 const initialGameState = {
   game_id: 0,
   player1: {},
   player2: {},
   score: {},
-  winner: 0
-}
+  winner: 0,
+};
 
 const initialPlayerState = {
   player_id: 0,
@@ -25,28 +22,24 @@ const initialPlayerState = {
   board: [],
   fleet: [],
   boats_floating: null,
-}
+};
 
-const initialFleetState = []
+const initialFleetState = [];
 
 const initialBoatState = {
   boat_name: "",
   boat_length: 0,
-  boat_status: [], // array containing the squares of the boat, containing hit/miss/null
-}
-
-
+  boat_status: [], // array containing the squares of the boat, containing hit/miss/open
+};
 
 function App() {
-  
-  
   return (
     <AppContainer>
-      <Title>BoatFight</Title>
-      <GameContainer>
-        <Board />
-        <GameInfoBox />
-      </GameContainer>
+      <Title>BoatFight!</Title>
+      <Routes>
+        <Route exact path="/" element={<LandingPage />}/>
+        <Route path="/game" element={<Game />}/>
+      </Routes>
     </AppContainer>
   );
 }
