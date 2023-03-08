6e6e086c112c2e07089d06f07e985f5b448f7601
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2023-03-08 11:20:34 -0600

    add MUI CssBaseline, remove css reset
    move index.css to styles/app.css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React, { createContext, useEffect, useState } from "react";
 import { Routes, Route } from "react-router";
 // Styles
 import { AppContainer, Title } from "./styles";
-import "./index.css";
+import { CssBaseline } from "@mui/material";
+import "./styles/app.css";
 // React Components
 import LandingPage from "./components/LandingPage";
 import Game from "./components/Game";
@@ -13,17 +14,20 @@ import initialState from "./state";
 export const GameContext = createContext();
 
 export default function App() {
-	const [game, setGame] = useState(initialState)
-	
+  const [game, setGame] = useState(initialState);
+
   return (
-    <GameContext.Provider value={{ game, setGame }}>
-      <AppContainer>
-        <Title>BoatFight!</Title>
-        <Routes>
-          <Route exact path="/" element={<LandingPage />} />
-          <Route path="/game" element={<Game />} />
-        </Routes>
-      </AppContainer>
-    </GameContext.Provider>
+    <>
+		<CssBaseline />
+		<GameContext.Provider value={{ game, setGame }}>
+			<AppContainer>
+				<Title>BoatFight!</Title>
+				<Routes>
+					<Route exact path="/" element={<LandingPage />} />
+					<Route path="/game" element={<Game />} />
+				</Routes>
+			</AppContainer>
+		</GameContext.Provider>
+    </>
   );
 }
