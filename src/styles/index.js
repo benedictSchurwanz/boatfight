75a31cfbf269d0cc5067a777f1ab439aa6841b43
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2023-03-08 22:05:18 -0600

    revise and refactor css in Styled Components

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,155 +1,144 @@
 import styled from "styled-components";
 
 export const AppContainer = styled.div`
-  // border: 10px solid gray; // for debugging
-
+  // border: 1px solid gray; 
+	background-color: lightgrey;
+	
   display: flex;
-  flex: 0 1 auto;
-
   flex-direction: column;
   align-items: center;
 `;
 
 export const Title = styled.h1`
-  // border: 1px solid gray; // for debugging
-
-  width: auto;
+  // border: 1px solid gray; 
+	
+	font-size: 2rem;
+	font-weight: bold;
+	
+	margin: 0.38rem;
 `;
+
 
 ///////// Game Display //////////////////////////
 
+
 export const GameContainer = styled.div`
-  // border: 1px solid gray; // for debugging
+  border: 1px solid black; 
 
-  display: flex;
-  flex-direction: row;
   width: 100vw;
+	
+  display: flex;
+  justify-content: space-evenly;
 
-  > * {
-    margin-left: 1rem;
-  }
+	> * {
+		margin: 0.38rem;
+	}
 `;
+
 
 ///////// Game Board ////////////////////////////
 
+
 export const BoardContainer = styled.div`
-  // border: 1px solid blue; // for debugging
+  border: 2px solid white; 
 
-  width: 61.8vw;
-
-  display: flex;
-  justify-content: center;
   background-color: lightblue;
   border-radius: 1rem;
-
-  padding: 1rem;
-  padding-bottom: 2rem;
-
-  * {
-    white-space: pre-wrap;
-    font-family: monospace;
-    font-size: 0.825rem;
-    line-height: 1.175rem;
-  }
-`;
-
-export const BoardGridContent = styled.div`
-  border: 1px solid red; // for debugging
+  width: 60vw;
 
   display: flex;
-  flex-direction: column;
-  justify-content: center;
-  width: 100%;
-  margin: 0rem;
-  font-size: 0.725rem;
-
-  > * {
-    // border: 1px solid red;
-
-  }
-`;
-
-export const GridRowContainer = styled.div`
-  // border: 1px solid gray; // for debugging
 	
-	margin: 0.38rem;
-	padding: 0.38rem;
+	padding: 1rem;
 `;
 
-export const GridCellSquare = styled.div`
-  border: 1px solid gray; // for debugging
+export const GridContainer = styled.div`
+  border: 2px solid blue; 
+	
+  width: 100%;
+  
+	display: flex;
+	// flex-direction: column;
+	// align-items: stretch; 
+	
+	margin: 0;
+	padding: 0;
+`;
 
-  width: 5%;
-  height: 2vw;
+export const RowContainer = styled.div`
+  border: 2px solid red;
+
+	// height: 1em;
+	
+  margin: 0;
+  padding: 0;
+`;
+
+export const Cell = styled.div`
+  border: 1px solid gray; 
+	
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-export const RowLabelContainer = styled.div``;
 
-///////// Game Info Box /////////////////////////
+///////// Game Info Box /////////////////
+
 
 export const GameInfoContainer = styled.div`
-  // border: 1px solid red; // for debugging
+  border: 1px solid red; 
 
-  width: 38.2vw;
-
+  background-color: lightblue;
+  border-radius: 1rem;
+	
+  width: 37vw;
   display: flex;
   flex-direction: column;
-  background-color: lightblue;
-  border-radius: 1em;
-
-  margin-right: 1rem;
-  padding-bottom: 2.2rem;
-
-  * {
-    white-space: pre-wrap;
-    font-family: monospace;
-    font-size: 0.825rem;
-    line-height: 1.175rem;
-  }
-
-  > * {
-    padding: 0.62rem;
-  }
+	
+  margin-left: 0;
 `;
 
 export const GameInfoHeader = styled.h2`
-	// border: 1px solid gray; // for debugging
+  border: 1px solid gray; 
 
   color: slategray;
-  font-size: 1.1rem;
-  padding: 1rem 2rem 0.5rem;
-  // padding-top: 0.5rem;
+	font-family: monospace;
+  font-size: 1.25rem;
+	text-decoration: underline;
+	
 	margin: 0;
-  margin-top; 1rem;
+	padding: 0.5rem 1.5rem 0;
+	
+	// display: flex;
+	// justify-content: center;
+	
 `;
 
-export const UL = styled.ul`
-  // border: 1px solid gray; // for debugging
+export const BoatListUL = styled.ul`
+  border: 1px solid gray; 
 
-  padding: 0.5rem 1rem 2rem 2rem;
-  margin: 0rem 1rem;
   border-bottom: 1px solid lightslategray;
+
+	margin: 0;
+	padding: 0.5rem 2rem 0;
 `;
+
+
+///////// Target Input Form /////////////
+
 
 export const FormContainer = styled.div`
-  // border: 1px solid red; // for debugging
+  border: 1px solid red; 
 
   display: flex;
   flex-direction: column;
-
-  margin: 0rem 1rem;
-  padding: 1.62rem;
-
-  * {
-    // border: 1px solid brown;
-    margin: 0;
-  }
+	
+	padding: 1rem;
 `;
 
 export const FireButton = styled.div`
+  border: 1px solid red; 
+
   display: flex;
   justify-content: center;
 `;
