e52a9ad25a49a3f88ee14c6bd9edc985be776a2b
Benedict Schurwanz <19333755+benedictSchurwanz@users.noreply.github.com>
2024-05-19 22:23:05 -0500

    testing mui styled components

---
+++ src/styles/muiStyles.js
@@ -0,0 +1,9 @@
+import { styled } from "@mui/system";
+import { Box, Stack } from "@mui/material";
+
+export const BoardBox = styled(Box)(() => ({
+	border: "2px solid blue",
+	width: 1,
+	margin: 0,
+	padding: 0
+}))
