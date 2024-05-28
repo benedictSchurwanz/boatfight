f902df0776865e261849467e326b036e604fb391
Benedict Schurwanz <19333755+benedictSchurwanz@users.noreply.github.com>
2024-05-28 13:08:27 -0500

    add title click for easier testing

--- src/App.js
+++ src/App.js
@@ -7,11 +7,23 @@ import "./styles/app.css";
 // React Components
 import LandingPage from "./components/LandingPage";
 import Game from "./components/Game";
+import { useGameSetup } from "./functions/setup";
+import { GameContext, PlayersContext, SetupContext } from "./state/contexts";
 
 export default function App() {
+	const { dispatch: setupdispatch } = useContext(SetupContext);
+  const { state: gamestate } = useContext(GameContext);
+  const { state: playerstate } = useContext(PlayersContext);
+
+	const useTitleClick = () => {
+		useGameSetup({gamestate, playerstate, setupdispatch})
+	}
+	
   return (
     <AppContainer>
-      <Title>BoatFight!</Title>
+      <Title
+				onClick={useTitleClick}
+			>BoatFight!</Title>
       <Routes>
         <Route exact path="/" element={<LandingPage />} />
         <Route path="/game" element={<Game />} />
