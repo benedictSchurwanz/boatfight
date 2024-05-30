b65b827f7c738625457537342b4c7a9bec34b46c
Benedict Schurwanz <19333755+benedictSchurwanz@users.noreply.github.com>
2024-05-30 18:45:40 -0500

    reconfigure refactor reducers

--- src/state/reducers.js
+++ src/state/reducers.js
@@ -1,4 +1,10 @@
-import { PLACE_BOATS_START, PLACE_BOAT_ILLEGAL, PLACE_BOAT_LEGAL, GAME_SETUP_START } from "./constants";
+import {
+  PLACE_BOATS_START,
+  PLACE_BOAT_ILLEGAL,
+  PLACE_BOAT_LEGAL,
+  GAME_SETUP_START,
+	PLACE_BOAT_ORIENTATION_FLIP,
+} from "./constants";
 
 export const gameReducer = (state, action) => {
   switch (action.type) {
@@ -23,37 +29,45 @@ export const boardReducer = (state, action) => {
 
 export const setupReducer = (state, action) => {
   switch (action.type) {
-    case PLACE_BOATS_START: {
-			const gamestate = action.payload.gamestate;
-			const playerstate = action.payload.playerstate;
-			const currentPlayer = playerstate[gamestate.currentPlayer]
-			
+    case GAME_SETUP_START: {
       return {
         ...state,
-				placeBoatsMsg: true,
-				placeBoatsGrayText: !state.placeBoatsGrayText,
-				currentBoatIndex: action.payload.boatIndex,
-				currentBoat: currentPlayer.fleet[action.payload.boatIndex]
+        status: true,
+        placeBoatHighlightColor: "red",
+        placeBoatIsHorizontal: true,
       };
     }
-		case PLACE_BOAT_LEGAL: {
-			return {
-				...state,
-				placeBoatHighlightColor: "green"
-			}
-		}
-		case PLACE_BOAT_ILLEGAL: {
-			return {
-				...state,
-				placeBoatHighlightColor: "red"
-			}
-		}
-    case GAME_SETUP_START:
-			return {
-				...state,
-        status: true,
-				placeBoatHighlightColor: "red"
+    case PLACE_BOATS_START: {
+      const gamestate = action.payload.gamestate;
+      const playerstate = action.payload.playerstate;
+      const currentPlayer = playerstate[gamestate.currentPlayer];
+
+      return {
+        ...state,
+        placeBoatsMsg: true,
+        placeBoatsGrayText: !state.placeBoatsGrayText,
+        currentBoatIndex: action.payload.boatIndex,
+        currentBoat: currentPlayer.fleet[action.payload.boatIndex],
       };
+    }
+    case PLACE_BOAT_LEGAL: {
+      return {
+        ...state,
+        placeBoatHighlightColor: "green",
+      };
+    }
+    case PLACE_BOAT_ILLEGAL: {
+      return {
+        ...state,
+        placeBoatHighlightColor: "red",
+      };
+    }
+		case PLACE_BOAT_ORIENTATION_FLIP: {
+			return {
+				...state,
+				placeBoatIsHorizontal: !state.placeBoatIsHorizontal
+			}
+		}
     default:
       return state;
   }
